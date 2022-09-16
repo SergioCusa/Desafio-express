41c558bd036459c174fd3ffb811568018e5bcdd8
Sergio Cusa <sergiocusa.91@gmail.com>
2022-09-16 01:22:52 -0300

    form.hbs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 const app = express()
 const productosRouter = require ("./productosRouter")
+const handlebars = require ("express-handlebars")
 
 
 
@@ -8,9 +9,24 @@ const productosRouter = require ("./productosRouter")
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use("/api/productos",productosRouter)
-app.use("/",express.static( __dirname + "/public"))
+// app.use("/",express.static( __dirname + "/public"))
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+     extname:"hbs",
+     layoutsDir: __dirname + "/views/layouts",
+     
+}));
+
+app.set("views","/views")
+app.set("view engine","hbs")
 
 
+
+app.get("/productos",(req,res)=>{
+    res.render("form")
+})
 
 
 
